80d7957743a625da38f5e1df3fe4e19e9a8f4eb4
JamesV-S <hamzvilelas@gmail.com>
2025-01-17 20:40:05 +0000

    Update usersetup.py

--- usersetup_file/usersetup.py
+++ usersetup_file/usersetup.py
@@ -4,6 +4,8 @@ import maya.cmds as cmds
 
 # basic setup:
 def basic_setup_message():
-    cmds.confirmDialog(title = 'startup', message='Maya is stating up', button=['OK'])
+    cmds.confirmDialog(title = 'startup', 
+                       message='Maya is stating up', 
+                       button=['OK'])
 
 basic_setup_message()
