2a4f09d7257906753a89795553a3c76234e92697
JamesV-S <hamzvilelas@gmail.com>
2025-01-17 20:57:55 +0000

    Update usersetup.py

--- usersetup_file/usersetup.py
+++ usersetup_file/usersetup.py
@@ -1,7 +1,10 @@
 
 # Jmvs user setup file
+import os
 import maya.cmds as cmds
-import logging
+import logging # debugging
+import subprocess # to run any batch scripts
+import urllib.request # download online files
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -20,4 +23,31 @@ def basic_setup_message():
                        button=['OK'])
 
 basic_setup_message()
+
+
+def run_jmvsShelf_Rigging_batch():
+    # provide url for bat file in git repo
+    url = "https://github.com/JamesV-S/JmvsShelf_Rigging/tree/main/batch_run_script/Jmvs_shelf_rigging.bat"
+    
+    # path for temp folder to download `.bat` to!
+    temp_script_path = os.path.join(os.getenv('TEMP'), 'Jmvs_shelf_rigging.bat')
+    
+    try:
+        # download batch file w/ python module `urlib.request` 
+        urllib.request.urlretrieve(url, temp_script_path)
+
+        # run the batch script
+        subprocess.call([temp_script_path], shell=True)
+    except Exception as e:
+        print(f"Error with downloading or running the batc script: {e}")
+
+run_jmvsShelf_Rigging_batch()
+
+
+def deferred_setup_tasks():
+    pass # Any tasks for AFTER the ui of maya has loaded.
+    print("Deferred setup done.")
+
+deferred_setup_tasks()
+
 
